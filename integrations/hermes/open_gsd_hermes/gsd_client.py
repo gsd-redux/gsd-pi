98c848cd165f18d32e1d496ef28939f69ed6e612
Jeremy McSpadden <jeremy@fluxlabs.net>
2026-08-29 11:55:57 -0500

    release: v1.17.0

--- integrations/hermes/open_gsd_hermes/gsd_client.py
+++ integrations/hermes/open_gsd_hermes/gsd_client.py
@@ -215,7 +215,7 @@ class GsdMcpClient:
                 "params": {
                     "protocolVersion": "2024-11-05",
                     "capabilities": {},
-                    "clientInfo": {"name": "open-gsd-hermes", "version": "1.16.2"},
+                    "clientInfo": {"name": "open-gsd-hermes", "version": "1.17.0"},
                 },
             }
         )
